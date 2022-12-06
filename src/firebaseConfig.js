3adbe5adb8ce2f62b44ef0abad0dcf0e1eede34b
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-06 09:55:29 +0200

    Add getAuth from firebase

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
@@ -11,4 +12,5 @@ const firebaseConfig = {
 };
 
 export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
 export const database = getFirestore(app);
