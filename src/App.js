f0558ea14839d8041463d5a60c12d3443ec15e95
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-06 09:45:32 +0200

    Add auth context

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import { Route, Routes } from 'react-router-dom';
 import { Login } from './components/auth/Login';
 import { Register } from './components/auth/Register';
+import { AuthContextProvider } from './contexts/AuthContext';
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path='/' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-      </Routes>
-    </div>
+    <AuthContextProvider>
+      <div className="App">
+        <Routes>
+          <Route path='/' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+        </Routes>
+      </div>
+    </AuthContextProvider>
   );
 }
 
