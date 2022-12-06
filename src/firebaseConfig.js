f1ec2f9ef0d4fa916ade1212afa38622510c5117
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-06 09:30:04 +0200

    Add firebase to project

---
+++ src/firebaseConfig.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAmquYzUR8l26dIZOndCb2vPdcfIAxkfnM",
+  authDomain: "fitness-app-ba6d1.firebaseapp.com",
+  projectId: "fitness-app-ba6d1",
+  storageBucket: "fitness-app-ba6d1.appspot.com",
+  messagingSenderId: "992420590503",
+  appId: "1:992420590503:web:b7d0312a82d7c9dc457c82"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const database = getFirestore(app);
