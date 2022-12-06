5718f6f1a91b5c2ee185675f68e1a9350f3be8e8
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-06 10:52:25 +0200

    Add possibility users to register

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,9 +1,40 @@
+import { Link, useNavigate } from "react-router-dom";
+import { browserLocalPersistence, setPersistence, signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from "../../firebaseConfig";
+
 export const Login = () => {
+    const navigate = useNavigate();
+
+    const onLogin = (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.target);
+
+        const email = formData.get('email');
+        const password = formData.get('password');
+
+        if (email === '' || password === '') {
+            alert('Please fill all the fields!');
+            return;
+        }
+
+        setPersistence(auth, browserLocalPersistence)
+            .then(() => {
+                signInWithEmailAndPassword(auth, email, password)
+                    .then(() => {
+                        navigate('/fitness');
+                    })
+                    .catch((err) => {
+                        alert(err.message);
+                    })
+            })
+    }
+
     return (
         <div className="auth">
             <div className="auth__container">
                 <h1>Fitness App</h1>
-                <form className="auth__form">
+                <form className="auth__form" onSubmit={onLogin}>
                     <label htmlFor="email" />
                     <input type="text" placeholder="Email" id="email" name="email" />
                     <label htmlFor="password" />
@@ -18,7 +49,7 @@ export const Login = () => {
             </div>
             <div className="auth__action">
                 <p>Don't have an account?</p>
-                <a href="/register">Sign up</a>
+                <Link to="/register">Sign up</Link>
             </div>
         </div>
     );
