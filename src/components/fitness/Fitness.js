f5a15d82f010ce3b3a65280f26ee867f6fe89421
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-07 17:18:58 +0200

    Add login button and image in fitness component

--- src/components/fitness/Fitness.js
+++ src/components/fitness/Fitness.js
@@ -1,14 +1,17 @@
 import { Link } from 'react-router-dom';
+import FITNESS from '../../assets/health.jpg';
 
 export const Fitness = () => {
     return (
         <div className="fitness">
             <section className="fitness__content">
                 <div className="fitness__home">
-                    <h3>Fitness App</h3>
                     <h1>Start Your Fitness Journey!</h1>
                     <p>Check how to achieve your goal</p>
-                    <button>Explore</button>
+                    <Link to='/login'><button>Start</button></Link>
+                </div>
+                <div className="fitness__img">
+                    <img src={FITNESS} alt="" />
                 </div>
             </section>
         </div>
