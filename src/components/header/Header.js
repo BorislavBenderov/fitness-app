f0728c12e39b02ec7456b9dee18f78ceeaa2b43c
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-07 17:17:30 +0200

    Add more navigation buttons

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,9 +4,10 @@ import { Logout } from "../auth/Logout";
 export const Header = () => {
     return (
         <header className="header__container">
+            <Link to='/'><h3>Fitness App</h3></Link>
             <ul className="nav">
                 <li>
-                    <Link to="/fitness">Home</Link>
+                    <Link to="/">Home</Link>
                 </li>
                 <li>
                     <Link to="/calories">Calories</Link>
@@ -16,6 +17,12 @@ export const Header = () => {
                 </li>
                 <li>
                     <Link to="/fitness-programs">Programs</Link>
+                </li>
+                <li>
+                    <Link to="/login">Login</Link>
+                </li>
+                <li>
+                    <Link to="/register">Register</Link>
                 </li>
                 <Logout />
             </ul>
