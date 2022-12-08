ea465337a13d2c46ba81303effdd968b6c34f001
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-08 08:15:10 +0200

    Show/hide buttons for logged or guest users in Header component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,30 +1,39 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { Logout } from "../auth/Logout";
+import { AuthContext } from '../../contexts/AuthContext';
 
 export const Header = () => {
+    const { loggedUser } = useContext(AuthContext);
     return (
         <header className="header__container">
             <Link to='/'><h3>Fitness App</h3></Link>
             <ul className="nav">
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/calories">Calories</Link>
-                </li>
-                <li>
-                    <Link to="/weight">Weight</Link>
-                </li>
-                <li>
-                    <Link to="/fitness-programs">Programs</Link>
-                </li>
-                <li>
-                    <Link to="/login">Login</Link>
-                </li>
-                <li>
-                    <Link to="/register">Register</Link>
-                </li>
-                <Logout />
+                {loggedUser
+                    ? <>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/calories">Calories</Link>
+                        </li>
+                        <li>
+                            <Link to="/weight">Weight</Link>
+                        </li>
+                        <li>
+                            <Link to="/fitness-programs">Programs</Link>
+                        </li>
+                        <Logout />
+                    </>
+                    : <>
+                        <li>
+                            <Link to="/login">Login</Link>
+                        </li>
+                        <li>
+                            <Link to="/register">Register</Link>
+                        </li>
+                    </>
+                }
             </ul>
         </header>
     );
