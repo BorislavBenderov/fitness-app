b3a3b06d1ba367c9bf81c7e36a549d81987c7f82
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-08 18:36:28 +0200

    Add view and download buttons in fitness programs component

--- src/components/fitness-programs/FitnessPrograms.js
+++ src/components/fitness-programs/FitnessPrograms.js
@@ -1,28 +1,35 @@
 import { Link } from "react-router-dom";
+import FULLBODYPROGRAM from '../../assets/fullbody-program.xlsx';
 
 export const FitnessPrograms = () => {
     return (
         <div className="fitness__programs">
             <h1>Choose a Program</h1>
             <section className="programs">
-                <Link to={`/fitness-programs/fullbody`}>
                     <div className="program__container" >
                         <h2>Full Body</h2>
                         <p>Choose this program if you do not have 2+ years of training</p>
+                        <div className="programs__btns">
+                            <a href={FULLBODYPROGRAM} download><button>Download Program</button></a>
+                            <Link to={`/fitness-programs/fullbody`}><button>View Program</button></Link>
+                        </div>
                     </div>
-                </Link>
-                <Link to={`/fitness-programs/upper-lower`}>
                     <div className="program__container">
                         <h2>Upper/Lower</h2>
                         <p>Choose this program if you have 2+ years of training</p>
+                        <div className="programs__btns">
+                            <a href={FULLBODYPROGRAM} download><button>Download Program</button></a>
+                            <Link to={`/fitness-programs/upper-lower`}><button>View Program</button></Link>
+                        </div>
                     </div>
-                </Link>
-                <Link to={`/fitness-programs/push-pull-legs`}>
                     <div className="program__container">
                         <h2>Push/Pull/Legs</h2>
                         <p>Choose this program if you have 4+ years of training</p>
+                        <div className="programs__btns">
+                            <a href={FULLBODYPROGRAM} download><button>Download Program</button></a>
+                            <Link to={`/fitness-programs/push-pull-legs`}><button>View Program</button></Link>
+                        </div>
                     </div>
-                </Link>
             </section>
         </div>
     );
