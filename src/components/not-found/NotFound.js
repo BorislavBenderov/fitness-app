586921784dfc0111140f166cf677b5949f5cd41f
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-14 10:26:34 +0200

    Add 404 page

---
+++ src/components/not-found/NotFound.js
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+export const NotFound = () => {
+    return (
+        <div className="not-found">
+            <div className="not-found-info">
+                <h2>404.That's an error.</h2>
+                <p>The requested page was not found on this server.</p>
+                <Link to='/'>
+                    <p>Go Home</p>
+                </Link>
+            </div>
+        </div>
+    );
+}
